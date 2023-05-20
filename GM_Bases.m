334b0983ac4a76aab25c0a5cf64ccb8a7814a8f6
BeshoyMorad <besh0morta@gmail.com>
2023-05-20 23:53:29 +0300

    Finish first function

--- GM_Bases.m
+++ GM_Bases.m
@@ -1,8 +1,9 @@
 function [phi1, phi2] = GM_Bases(s1, s2)
-	% Calculate the first basis vector
-	phi1 = s1 / norm(s1);
+	phi1 = s1 / sqrt(dot(s1, s1));
 	
-	phi2 = s2;
-	phi2 = phi2 - dot(phi1, phi2) * phi1;
-	phi2 = phi2 / norm(phi2);
+	v2 = s2 - dot(s2, phi1) * phi1;
+	phi2 = v2 / sqrt(dot(v2, v2));
+	
+	phi1 = phi1 * sqrt(length(s1));
+	phi2 = phi2 * sqrt(length(s2));
 end
